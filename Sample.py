539a88bd1be5e24cc81ff8dcfb56847c48979490
redispriya-netizen <redispriya@gmail.com>
2026-08-10 16:42:46 -0400

    Updated user input message

--- Sample.py
+++ Sample.py
@@ -1,5 +1,5 @@
 # 1. Take username input from the user
-username = input("Enter your username: ")
+username = input("Enter your username First and Last name: ")
 
 # 2. Print the name inside a hello world greeting
 print(f"Hello, {username}! Welcome to the world.")
